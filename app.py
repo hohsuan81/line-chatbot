ffe6258a3abda56af582ad2c99b1c6931f0dff35
hohsuan81 <istars1024@gmail.com>
2025-06-03 20:52:25 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, abort
+from linebot.v3 import WebhookParser
+from linebot.v3.messaging import MessagingApi, ReplyMessageRequest, TextMessage
+from linebot.v3.messaging import Configuration, ApiClient
+
+app = Flask(__name__)
+
+# 把這裡的 Channel Access Token 和 Secret 換成你自己的
+channel_access_token = 'MVLD4+R4qzkb2QRUFCyYwO/ZP0vS84eZYjxDQkOLZpagFszusQUHpB01Woz50bU0uVAZNy4MOUeMqtc02OmLq+vH1ke6UdPOB2ipO9LqC0O1w/ZS6jaQi4xe88i+yS4vxHEKsaeI+35wv8cJmogpJAdB04t89/1O/w1cDnyilFU='
+channel_secret = 'c00b5bc4269190998e8e5e1bde9f9e6b'
+
+configuration = Configuration(access_token=channel_access_token)
+
+# parser 負責驗證簽名
+parser = WebhookParser(channel_secret)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        events = parser.parse(body, signature)
+    except Exception as e:
+        print("驗證錯誤：", e)
+        abort(400)
+
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+
+        for event in events:
+            if event.type == "message" and event.message.type == "text":
+                reply_token = event.reply_token
+                user_text = event.message.text
+
+                message = TextMessage(text=user_text)
+                req = ReplyMessageRequest(reply_token=reply_token, messages=[message])
+                line_bot_api.reply_message(req)
+
+    return 'OK'
+
+if __name__ == "__main__":
+    app.run()
